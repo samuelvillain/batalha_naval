55a90ad93e180255f3ab386de1dc4efcc42f3e23
Samuel Villain <samuelsvillain@gmail.com>
2023-06-20 17:33:13 -0300

    primeira matriz pronta

---
+++ main.py
@@ -0,0 +1,11 @@
+def matriz():
+    jogo = [["Água", "Água", "Água", "Água", "Água", "Água", "Água", "Água", "Água"],
+            ["Água", "Navio", "Água", "Navio", "Água", "Água", "Água", "Água", "Água"],
+            ["Água", "Água", "Água", "Navio", "Água", "Água", "Água", "Água", "Água"],
+            ["Água", "Água", "Água", "Água", "Navio", "Água", "Água", "Água", "Água"],
+            ["Água", "Água", "Água", "Água", "Água", "Água", "Água", "Água", "Água"],
+            ["Água", "Navio", "Água", "Navio", "Água", "Água", "Água", "Água", "Navio"],
+            ["Navio", "Água", "Navio", "Água", "Água", "Navio", "Água", "Navio", "Água"],
+            ["Água", "Água", "Navio", "Navio", "Água", "Água", "Navio", "Água", "Navio"],
+            ["Navio", "Navio", "Água", "Água", "Navio", "Navio", "Água", "Água", "Navio"]]
+    return jogo
