b4b4e9e22735dd0179c63014605b8ff789d619c8
Samuel Villain <samuelsvillain@gmail.com>
2023-06-21 12:33:44 -0300

    segunda matriz no dia a dia

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def matriz():
+def matriz_respostas():
     jogo = [["Água", "Água", "Água", "Água", "Água", "Água", "Água", "Água", "Água"],
             ["Água", "Navio", "Água", "Navio", "Água", "Água", "Água", "Água", "Água"],
             ["Água", "Água", "Água", "Navio", "Água", "Água", "Água", "Água", "Água"],
@@ -9,3 +9,32 @@ def matriz():
             ["Água", "Água", "Navio", "Navio", "Água", "Água", "Navio", "Água", "Navio"],
             ["Navio", "Navio", "Água", "Água", "Navio", "Navio", "Água", "Água", "Navio"]]
     return jogo
+
+def matriz_ataque():
+    jogo_ataque = [["*", "*", "*", "*", "*", "*", "*", "*", "*"],
+                   ["*", "*", "*", "*", "*", "*", "*", "*", "*"],
+                   ["*", "*", "*", "*", "*", "*", "*", "*", "*"],
+                   ["*", "*", "*", "*", "*", "*", "*", "*", "*"],
+                   ["*", "*", "*", "*", "*", "*", "*", "*", "*"],
+                   ["*", "*", "*", "*", "*", "*", "*", "*", "*"],
+                   ["*", "*", "*", "*", "*", "*", "*", "*", "*"],
+                   ["*", "*", "*", "*", "*", "*", "*", "*", "*"],
+                   ["*", "*", "*", "*", "*", "*", "*", "*", "*"]]
+
+    return jogo_ataque
+
+while True:
+    while True:
+        while True:
+            linha = int(input("Digite o número da linha que deseja: "))
+            if linha > 8:
+                print("Valor %d inválido. Digite um número de 0 a 8." % linha)
+                continue
+            break
+
+        while True:
+            coluna = int(input("Digite o número da coluna que deseja: "))
+            if coluna > 8:
+                print("Valor %d inválido. Digite um número de 0 a 8." % coluna)
+                continue
+            break
